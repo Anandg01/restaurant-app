105f2d493575d5d5bd386131b36d3c967c8eba7c
Anandg01 <ptanandg@gmail.com>
2024-04-15 23:30:44 +0530

    Making the Items visible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header/Header";
 import MealsSummary from "./components/Meals/MealsSummary";
+import MealsItems from "./components/Meals/MealsItems";
 function App() {
   return (
     <div>
       <Header />
       <MealsSummary />
+      <MealsItems />
     </div>
   );
 }
